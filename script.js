1ea0da58ea383065d2f99ae818ed904b65b18b7d
Aman <ubica33@mail.ru>
2021-09-26 13:02:17 +0600

    Release version 1.0

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // let apiQuotes = [];
 
+// show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // show new quote
 function newQuote() {
+    loading();
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 // Check If Author field is blank and replace it with 'Unknown'
  if(quote.author){ 
@@ -21,21 +35,24 @@ if(quote.text.length > 120){
 } else {
     quoteText.classList.remove('long-quote');
 }
+// Set quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes from API
 
-// async function getQuotes() {
-//     const apiUrl = 'https://type.fit/api/quotes';
-//     try {
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//     } catch (error){
-//         alert(error);
-//     }
-// }
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error){
+        alert(error);
+    }
+}
 
 // Tweet Quote
 function tweetQuote() {
@@ -48,5 +65,5 @@ newQuoteBtn.addEventListener('click', newQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
-// getQuotes();
-newQuote();
+ getQuotes();
+//newQuote();
